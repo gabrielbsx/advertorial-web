b3459be9eefeab5189f7ad0754d88dbac1f772dd
Gabriel <gabrielturing@gmail.com>
2022-08-03 22:26:45 -0300

    remove

--- g1/pages/index.js
+++ g1/pages/index.js
@@ -1,15 +1,17 @@
+import Image from 'next/image';
+
 function Home() {
   return (
     <>
-      <div className="text-center">
-        <>
-          Menu
-        </>
-        <>
+      <div>
+        <div className="bg-gray-700 text-white py-4 text-center">
+          <Image src={'/menu-left.png'} width="100px" height="100px" alt="menu-left" />
+        </div>
+        <div className="bg-gray-800 text-white py-10 text-center">
           Header
-        </>
+        </div>
       </div>
-      <div className="text-center">
+      <div className="text-center text-white py-20 sm:py-2 bg-gray-900">
         <>
           Section of posts
         </>
