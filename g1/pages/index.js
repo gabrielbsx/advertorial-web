47ed906fcd8aaf34b6ff4c51d5f0df3176be3953
Gabriel <gabrielturing@gmail.com>
2022-08-03 21:35:11 -0300

    partials

--- g1/pages/index.js
+++ g1/pages/index.js
@@ -1,8 +1,26 @@
 function Home() {
   return (
-    <div className="text-center">
-      Index
-    </div>
+    <>
+      <div className="text-center">
+        <>
+          Menu
+        </>
+        <>
+          Header
+        </>
+      </div>
+      <div className="text-center">
+        <>
+          Section of posts
+        </>
+        <>
+          Publish
+        </>
+      </div>
+      <div className="text-center">
+        Footer
+      </div>
+    </>
   );
 }
 
